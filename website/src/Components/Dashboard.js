abb3fdf1aa43bb9e081956e3ce72c5949a40cdd5
Garin Mccullick <glmccullick@gmail.com>
2021-08-06 08:53:38 -0500

    added projects page and fixed links

--- website/src/Components/Dashboard.js
+++ website/src/Components/Dashboard.js
@@ -2,16 +2,21 @@ import React, { Component } from 'react';
 import '../App.css';
 import Cards from './Cards';
 import Form from './Form';
+import Projects from './Projects';
 
 class Dashboard extends Component {
 
-    state ={
+    state ={ 
+        showCards: true,
         showForm: false,
+        showProjects: false,
     }
 
     handleHomeClick = () => {
         this.setState(({
-            showForm: false,
+            showCards: true,
+            showProjects:false,
+            showForm:false,
           })
           );
     }
@@ -19,16 +24,29 @@ class Dashboard extends Component {
     handleFormClick = () => {
         this.setState(({
             showForm: true,
+            showCards:false,
+            showProjects:false,
+          })
+          );
+    }
+
+    handleProjectClick = () => {
+        this.setState(({
+            showProjects: true,
+            showCards:false,
+            showForm:false,
           })
           );
     }
 
     render() {
         let display
-        if(this.state.showForm){
+        if(this.state.showForm === true){
             display = <Form />
-        }else {
+        }else if(this.state.showCards === true) {
             display = <Cards />
+        }else if(this.state.showProjects === true) {
+            display = <Projects />
         }
 
     return(
@@ -43,13 +61,16 @@ class Dashboard extends Component {
             
             <div className='navigation'>
                 <div className='link' onClick={this.handleHomeClick}>Home</div>
-                <div className='link'>About Me</div>
-                <div className="link">Education</div>
-                <div className="link">Experience</div>
-                <div className="link">Skills</div>
+                <div className='link' onClick={this.handleProjectClick}>Projects</div>
             </div>
             <div className='contact'>
                 <button className='call-to-actionBtn' onClick={this.handleFormClick}>Want to talk?</button>
+                <div className='personal-info'>
+               <div className='personal-left'>
+                    <p>tel: (573) 680-8460</p>
+                    <p>glmccullick@gmail.com</p>
+               </div>
+            </div>
             </div>
         </div>
         <div className='right-side'>
---
+++ website/src/Components/Projects.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import '../Projects.css'
+
+
+class Projects extends Component {
+    render(){
+        return(
+            <div className='wrapper'>
+                <h1>Projects</h1>
+                <div className='card-wrapper'>
+                    <div className='card'>
+                        <div className='top-card'>
+
+                        </div>
+                    </div>
+                    <div className='card'>
+                        <div className='top-card'>
+
+                        </div>
+                    </div>
+                    <div className='card'>
+                        <div className='top-card'>
+
+                        </div>
+                    </div>
+                </div>
+                <div className='skill-bar'>
+                    <i class="fab fa-css3-alt"></i>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Projects
